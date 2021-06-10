c48d621679e1905e997d3e41db9323140f2770f9
update <timife007@gmail.com>
2021-06-10 12:58:20 -0400

    dashboard start

--- app/src/main/java/com/timife/agromall/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/timife/agromall/dashboard/DashboardFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import com.timife.agromall.R
+import com.timife.agromall.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
+    private lateinit var binding : FragmentDashboardBinding
 
     companion object {
         fun newInstance() = DashboardFragment()
@@ -18,13 +22,23 @@ class DashboardFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding = FragmentDashboardBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.pieChart.observe(viewLifecycleOwner) {
+            binding.pieChart.data = it
+            binding.pieChart.transparentCircleRadius = 0F
+            binding.pieChart.setDrawEntryLabels(false)
+            binding.pieChart.holeRadius = 60F
+            binding.pieChart.invalidate()
+        }
+
+        binding.farmersCard.setOnClickListener {
+            this.findNavController().navigate(R.id.action_dashboardFragment_to_farmersFragment)
+
+        }
+
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/timife/agromall/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/timife/agromall/dashboard/DashboardViewModel.kt
@@ -1,7 +1,38 @@
 package com.timife.agromall.dashboard
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class DashboardViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _pieChart: MutableLiveData<PieData> = MutableLiveData()
+    val pieChart : LiveData<PieData>
+        get() = _pieChart
+
+    init {
+//        salesBarData()
+//        transactionsBarData()
+        pieDataEntry()
+    }
+
+    private fun pieDataEntry(): ArrayList<PieEntry>{
+        val colorClassArray = listOf(ColorTemplate.rgb("#4E2433"),ColorTemplate.rgb("#274E24"),ColorTemplate.rgb("#50CF46"),ColorTemplate.rgb("#3245F4"),ColorTemplate.rgb("#CD801F"))
+
+        val dataVals : ArrayList<PieEntry> = ArrayList()
+        dataVals.add(PieEntry(10F, "Subsistence"))
+        dataVals.add(PieEntry(7F,"Commercial"))
+        dataVals.add(PieEntry(3F, "Home"))
+        dataVals.add(PieEntry(15F,"Container"))
+        dataVals.add(PieEntry(10F,"Vertical"))
+        val pieDataSet = PieDataSet(dataVals,"Series PieChart")
+        pieDataSet.colors = colorClassArray
+        val pieData = PieData(pieDataSet)
+        _pieChart.value = pieData
+        return dataVals
+
+    }
 }
